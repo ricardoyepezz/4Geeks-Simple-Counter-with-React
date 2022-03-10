c21750d75a5704ac6bde5da01105d7269e338e4a
ricardoyepezz <ricardoyepezz@gmail.com>
2022-03-10 19:38:28 +0000

    exercise done

--- src/js/component/secondsCounter.jsx
+++ src/js/component/secondsCounter.jsx
@@ -1,15 +1,28 @@
+import PropTypes from "prop-types";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
 export const SecondsCounter = (props) => {
 	return (
-		<>
-			<i clasname="fas fa-clock"></i>
-			<label>0</label>
-			<label>0</label>
-			<label>0</label>
-			<label>0</label>
-			<label>0</label>
-			<label>{props.counter}</label>
-		</>
+		<div className="bigContainer">
+			<div className="icon">
+				<FontAwesomeIcon icon={faClock} />
+			</div>
+			{/* para evitar dos digitos en un div utilizo el residuo de la división de cada numero entre 10 */}
+			<div>{props.numberFive % 10}</div>
+			<div>{props.numberFour % 10}</div>
+			<div>{props.numberThree % 10}</div>
+			<div>{props.numberTwo % 10}</div>
+			<div>{props.numberOne % 10}</div>
+		</div>
 	);
+};
+
+SecondsCounter.propTypes = {
+	numberFive: PropTypes.number,
+	numberFour: PropTypes.number,
+	numberThree: PropTypes.number,
+	numberTwo: PropTypes.number,
+	numberOne: PropTypes.number,
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,30 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
 import { SecondsCounter } from "./component/secondsCounter.jsx";
 
-//render your react application
 let counter = 0;
 setInterval(() => {
-	counter += 1;
-	console.log(counter);
+	//math.floor para redondear hacia abajo la división de counter entre xxx
+	const five = Math.floor(counter / 10000);
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	counter++;
+	//render
 	ReactDOM.render(
-		<SecondsCounter seconds={counter} />,
+		<SecondsCounter
+			numberOne={one}
+			numberTwo={two}
+			numberThree={three}
+			numberFour={four}
+			numberFive={five}
+		/>,
 		document.querySelector("#app")
 	);
 }, 1000);
